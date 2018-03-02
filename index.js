878c1935dea9f48b7d6b468038824c025f3c0066
Sonia  D. Nicas <sduarte22@gmail.com>
2018-03-02 18:26:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ function updateObjectWithKeyAndValue(object ,key , value) {
   
 }
 
-
-function updateObjectWithKeyAndValue(object, key, value) {
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 object[key]=value;
 return object ;
 }
